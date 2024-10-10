e4845c8e712f97e2abee31e78d1a178b2e1762db
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-10 23:20:05 +0600

    profile detail html is ready

---
+++ src/assets/index.ts
@@ -0,0 +1,39 @@
+export { ReactComponent as DevLinkLGSVG } from "./images/logo-devlinks-large.svg";
+export { ReactComponent as EmailIconSVG } from "./images/icon-email.svg";
+export { ReactComponent as PasswordIconSVG } from "./images/icon-password.svg";
+
+export { ReactComponent as LinkIcon } from "./images/icon-link.svg";
+export { ReactComponent as GithubIcon } from "./images/icon-github.svg";
+export { ReactComponent as TwitchIcon } from "./images/icon-twitch.svg";
+export { ReactComponent as TwitterIcon } from "./images/icon-twitter.svg";
+export { ReactComponent as YoutubeIcon } from "./images/icon-youtube.svg";
+export { ReactComponent as HashnodeIcon } from "./images/icon-hashnode.svg";
+
+export { ReactComponent as LinkHeaderIcon } from "./images/icon-links-header.svg";
+export { ReactComponent as ProfileDetailsHeaderIcon } from "./images/icon-profile-details-header.svg";
+
+export { ReactComponent as IllustrationPhoneMockup } from "./images/illustration-phone-mockup.svg";
+export { ReactComponent as IllustrationEmpty } from "./images/illustration-empty.svg";
+
+export { ReactComponent as DragAndDropIcon } from "./images/icon-drag-and-drop.svg";
+export { ReactComponent as FrontendMentorIcon } from "./images/icon-frontend-mentor.svg";
+export { ReactComponent as LinkedinIcon } from "./images/icon-linkedin.svg";
+export { ReactComponent as FacebookIcon } from "./images/icon-facebook.svg";
+export { ReactComponent as DevtoIcon } from "./images/icon-devto.svg";
+export { ReactComponent as CodewarsIcon } from "./images/icon-codewars.svg";
+export { ReactComponent as CodepenIcon } from "./images/icon-codepen.svg";
+export { ReactComponent as FreeCodeCampIcon } from "./images/icon-freecodecamp.svg";
+export { ReactComponent as GitLabIcon } from "./images/icon-gitlab.svg";
+export { ReactComponent as StackOverflowIcon } from "./images/icon-stack-overflow.svg";
+
+export { ReactComponent as RightArrowIcon } from "./images/icon-arrow-right.svg";
+export { ReactComponent as UploadImageIcon } from "./images/icon-upload-image.svg";
+
+export { ReactComponent as LoadingIcon } from "./images/icon-loading.svg";
+export { ReactComponent as ErrorIcon } from "./images/icon-error.svg";
+export { ReactComponent as SuccessIcon } from "./images/icon-success.svg";
+
+export { ReactComponent as DevLinkSMSVG } from "./images/logo-devlinks-small.svg";
+export { ReactComponent as PreviewHeaderIcon } from "./images/icon-preview-header.svg";
+
+export * from "./data";
